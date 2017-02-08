f287cf61ad1935f55b75fabff0e23cb5ed1e2274
gkhollingshead <ghollingshead@incomm.com>
2017-02-08 11:07:53 -0800

    Set home html apart

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,18 +2,6 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: "home",
-  template: `
-<div class="pg-header">
-  <div class="container-1170">
-    <div layout="column" layout-align="center center" class="splash">
-      <img src="assets/img/RADMIN_logo.png">
-      <h5>&#34;WE PUT THE RAD IN RADMIN&#34;</h5>
-      <p>
-        <i class="material-icons">pets</i>
-      </p>
-    </div>
-  </div>
-</div>
-`
+  templateUrl: "home.html"
 })
 export class HomeComponent {}
